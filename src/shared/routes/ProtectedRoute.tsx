bc18b3bcc21e113b73e0aa5a60b28013ded8eb0f
Roman <asdwka3@gmail.com>
2025-02-01 14:49:23 +0100

    add LoadingCircle to public and protected rote

--- src/shared/routes/ProtectedRoute.tsx
+++ src/shared/routes/ProtectedRoute.tsx
@@ -1,12 +1,14 @@
 import { Outlet, Navigate } from 'react-router-dom'
 import { ROUTE_PATHS } from '../constants/routePaths';
 
-import { useAuthStore } from '../../features/auth/store/authStore'
+import { useAuthStore } from '../../features/auth/store/authStore';
+
+import { LoadingCircle } from '../components/LoadingCircle';
 
 export const ProtectedRoute = () => {
     const {isLoading, user, isAuthInitialized } = useAuthStore();
 
-    if (!isAuthInitialized || isLoading) return <div>loading...</div>;
+    if (!isAuthInitialized || isLoading) return <LoadingCircle />;
     if(!user) return <Navigate to={ROUTE_PATHS.SIGN_IN} /> ;
 
     return <Outlet />;
--- src/shared/routes/PublicRote.tsx
+++ src/shared/routes/PublicRote.tsx
@@ -2,10 +2,13 @@ import { Outlet, Navigate } from 'react-router-dom';
 import { useAuthStore } from '../../features/auth/store/authStore';
 import { ROUTE_PATHS } from '../constants/routePaths';
 
-export const PublicRoute = () => {
-    const { user, isAuthInitialized} = useAuthStore();
+import { LoadingCircle } from '../components/LoadingCircle';
 
-    if (!isAuthInitialized ) return <div>loading...</div>;
+
+export const PublicRoute = () => {
+    const { user, isAuthInitialized, isLoading} = useAuthStore();
+
+    if (!isAuthInitialized || isLoading ) return <LoadingCircle />;
 
     if (user) return <Navigate to={ROUTE_PATHS.HOME} />;
 
