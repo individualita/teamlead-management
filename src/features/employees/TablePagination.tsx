09da4f34414cf7713152d9bdd77daa7dee610d98
Roman <asdwka3@gmail.com>
2025-02-24 22:42:13 +0100

    refactor(table): improve naming, semantics, and pagination component

--- src/features/employees/TablePagination.tsx
+++
@@ -1,5 +0,0 @@
-const TablePagination = () => {
-    
-}
-
-export default TablePagination;
--- src/features/employees/components/employeesTable/EmployeesTable.tsx
+++ src/features/employees/components/employeesTable/EmployeesTable.tsx
@@ -1,41 +1,47 @@
-import Table from '../../../../shared/components/layouts/table/Table';
 
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 import { useEmployeeStore } from '../../../../shared/stores/employeesStore';
+
+import Table from '../../../../shared/components/layouts/table/Table';
+
+import TablePagination from '../../../../shared/components/layouts/tablePagination/TablePagination';
+
+
 
 const EmployeesTable = () => {
     
     const [rowsPerPage, setRowsPerPage] = useState<number>(5);
-
     const [currentPage, setCurrentPage] = useState<number>(1);
 
+    //get employees from global state.
     const {employees, deleteEmployee} = useEmployeeStore();
 
-    /* const employeeTableColumns = [... new Set(EMPLOYEES.flatMap(item => Object.keys(item)))];*/
-    const employeeTableColumns = employees.length? Object.keys(employees[0]).filter(item => item!== '_id'): [];
-
-
     const totalRows = employees.length;
+
+    /* const employeeTableColumns = [... new Set(EMPLOYEES.flatMap(item => Object.keys(item)))];*/
+
+    //calculate table columns excluding the id field. 
+    const employeeTableColumns = useMemo(() => {
+        return employees.length? Object.keys(employees[0]).filter(item => item!== '_id'): [];
+    }, [employees]);
 
 
     let startIndex = (currentPage - 1) * rowsPerPage; 
     let endIndex = Math.min((currentPage * rowsPerPage), totalRows);// не выходим за пределы totalRows
 
-
-    const showEmployeesRow = employees.slice(startIndex, endIndex); 
-
-
-    const isLastPage = endIndex >= totalRows; 
     const isFirstPage = currentPage === 1;
+    const isLastPage = endIndex >= totalRows; 
+
+    const displayedEmployees = employees.slice(startIndex, endIndex); 
+
     
-    const goToNextPage = () => {
+    const handleNextPage = () => {
         if(isLastPage) return; 
         setCurrentPage(prev => prev + 1);
-
     };
 
-    const goToPrevPage = () => {
+    const handlePrevPage = () => {
         if(isFirstPage) return;
         setCurrentPage(prev => prev -1);
 
@@ -44,51 +50,28 @@ const EmployeesTable = () => {
     const handleRowsChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setRowsPerPage(Number(e.target.value));
         setCurrentPage(1);
-    }
+    };
     
     return (
         <>
             <Table 
                 columns={employeeTableColumns} 
-                data={showEmployeesRow} 
+                data={displayedEmployees} 
                 onDelete={deleteEmployee}
             />
 
-            <div className='pagin flex justify-between bg-blue-50 p-2'>
-                <div className='select'>
-
-                    <label htmlFor='rows'>
-                        select rows
-                        <select name='rows' id='rows' onChange={handleRowsChange}>
-                            <option value='5'>5</option>
-                            <option value='10'>10</option>
-                            <option value={totalRows}>all</option>
-                        </select>
-                    </label>
-                    <div>StartIndex: {startIndex} endIndex: {endIndex}</div>
-                    <div>rows per page state: {rowsPerPage}</div>
-                    <div>current page state: {currentPage}</div>
-
-                </div>
-                
-                <h3>{startIndex + 1} - {endIndex} of {totalRows}</h3>
-                <div className='pages flex gap-5 text-bold text-xl '>
-                    <button 
-                        onClick={goToPrevPage}
-                        disabled={isFirstPage} 
-                        className={isFirstPage ? 'text-gray-400 cursor-not-allowed' : 'cursor-pointer'}
-                        >
-                            {'<'}
-                        </button>
-                    <button 
-                        onClick={goToNextPage}
-                        disabled={isLastPage} 
-                        className={`${isLastPage? 'text-gray-400 cursor-not-allowed': 'cursor-pointer'}`}
-                        >
-                            {'>'}
-                    </button>
-                </div>
-            </div>
+            <TablePagination 
+                onNext={handleNextPage} 
+                onPrev={handlePrevPage}
+                rowsPerPageOptions={[5, 10]}
+                rowsPerPage={rowsPerPage}
+                onRowsChange={handleRowsChange}
+                totalRows={totalRows}
+                isFirstPage={isFirstPage}
+                isLastPage={isLastPage}
+                endIndex={endIndex}
+                startIndex={startIndex}
+            />
 
         </>
     )
